01d68aef0b8bc6301dac3c9c08838bbfa2235321
nicola.valentini <nicola.valentini@emeal.nttdata.com>
2026-01-26 11:36:22 +0100

    feat: improve structure

--- app/global-not-found.tsx
+++ app/global-not-found.tsx
@@ -1,4 +1,17 @@
 import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+
+import './globals.css';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
 
 export const metadata: Metadata = {
   title: '404 - Page Not Found',
@@ -8,7 +21,7 @@ export const metadata: Metadata = {
 export default function GlobalNotFound() {
   return (
     <html lang='en'>
-      <body>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <div>
           <h1>404 - Page Not Found</h1>
           <p>The page you are looking for does not exist.</p>
